2fb318c0740809642c161daef95e67d57aebb99c
jotroick <jotroick@gmail.com>
2013-05-28 21:18:36 +0200

    cleanup and fix

--- project/ofdm_experiment_acoustic_channel.m
+++ project/ofdm_experiment_acoustic_channel.m
@@ -1,6 +1,6 @@
 % set parameter 
 %%%%%%%%%%%%%%%%
-snr = input('Enter the signal to noise ratio: ');
+%snr = input('Enter the signal to noise ratio: ');
 fs = 16000; % sampling frequency
 t_sim = 5;  % simulation time (measure frequency missmatch and noise)
 Nt = 5;     % number of training frames per package
@@ -33,7 +33,7 @@ Ncp = L + 10;               % calculation of cyclic prefix size
 % get bitstream from input file
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 img_in = imread('img/image_in.jpg');
-[img_row img_col img_color] = size(img_in);
+[img_row, img_col, img_color] = size(img_in);
 img_bin_in = de2bi(img_in, 8);
 x = img_bin_in(:);
 
@@ -50,10 +50,10 @@ Ns_train = Nt*Nn*M;
 x_train = round(rand(1, Ns_train));
 
 % qam modulation of data stream
-[y_qam_l lut] = qam_mod(x_norm, M);
+[y_qam_l, lut_t] = qam_mod(x_norm, M);
 
 % qam modulation of training stream
-[y_qam_t lut] = qam_mod(x_train, M);
+[y_qam_t, lut] = qam_mod(x_train, M);
 
 % generation of ofdm package
 Np_tot = length(y_qam_l)/(Nn*Nl);       % number of total packages
@@ -70,37 +70,37 @@ y_pack = [zeros(1,Nf_tot); y_pack_cut; zeros(1,Nf_tot);...
     flipud(conj(y_pack_cut))];          % consecutive packages ofdm
 
 % ofdm modulation
-[data_in y_ifft] = ofdm_mod(y_pack, Ncp);
+[data_in, y_ifft] = ofdm_mod(y_pack, Ncp);
 
 % send through the channel
 %sim('audio_io.mdl');
-%data_out = acoustic_channel_tv(data_in);
-
-% add noise
-y_n = awgn(data_in, snr);
-
-% add channel characteristics
-coeff = randn(1,L);
-data_out = filter(coeff, 1, y_n);
+data_out = acoustic_channel_tv(data_in);
+% 
+% % add noise
+% y_n = awgn(data_in, snr);
+% 
+% % add channel characteristics
+% coeff = randn(1,L);
+% data_out = filter(coeff, 1, y_n);
 
 % ofdm demodulation
-[y_demod_ofdm y_resh y_cut] = ofdm_demod(data_out, Nn, Nl, Ncp);
+[y_demod_ofdm, y_resh, y_cut] = ofdm_demod(data_out, Nn, Nl, Ncp);
 
 % equalize
 y_comp = eq_zf(y_demod_ofdm, y_block_t, Nn, Nt, Nl);
 
 % qam demodulation
-[y y_qam_r] = qam_demod(y_comp, M);
+[y, y_qam_r] = qam_demod(y_comp, M);
 
 % calculate bit error rate (BER)
-[bit_cnt bit_err_cnt ratio] = ber(x_norm, y);
+[bit_cnt, bit_err_cnt, ratio] = ber(x_norm, y);
 fprintf('the bit error ratio (BER) is: %d/%d=%f\n',...
     bit_err_cnt, bit_cnt, ratio);
 
 y_final = y(1:end-zero_count);
 
 % calculate symbol error rate (SER)
-[symb_cnt symb_err_cnt ratio] = ser(y_qam_l.', y_qam_r(:));
+[symb_cnt, symb_err_cnt, ratio] = ser(y_qam_l.', y_qam_r(:));
 fprintf('the symbol error ratio (SER) is: %d/%d=%f\n',...
     symb_err_cnt, symb_cnt, ratio);
 
@@ -109,4 +109,4 @@ fprintf('the symbol error ratio (SER) is: %d/%d=%f\n',...
 img_bin_out = reshape(y_final, [], 8);
 img_dec_out = bi2de(img_bin_out);
 img_res_out = reshape(img_dec_out, img_row, img_col, img_color);
-imwrite(img_res_out, 'img/image_out.jpg');
+imwrite(uint8(img_res_out), 'img/image_out.jpg');
