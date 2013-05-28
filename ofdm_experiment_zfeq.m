8b0114f51b2ddc5107ffebdfa7d147cd6d6802f8
Simon Maurer <moirelein@gmail.com>
2013-05-28 10:36:57 +0200

    equalizer as a function

--- ofdm_experiment_zfeq.m
+++ ofdm_experiment_zfeq.m
@@ -57,33 +57,8 @@ y_ch = filter(coeff, 1, y_n);
 % ofdm demodulation
 [y_demod_ofdm y_resh y_cut] = ofdm_demod(y_ch, Nn, Nl, Ncp);
 
-%%%%%%%%%%%%%%%%%%%
-% Equalizer Start %
-
-% remove redundant parts of ofdm package
-y_demod_ofdm_cut = y_demod_ofdm(2:Nn+1,:);          % Nn by Np_tot*(Nt+Nl)
-
-% reshape received packages
-y_dor = reshape(permute(...
-    reshape(y_demod_ofdm_cut, Nn, Nt+Nl, []), [1 3 2]), [], Nt+Nl);
-y_dor_t = y_dor(:, 1:Nt);                           % Nt by Np_tot*Nn
-y_dor_l = y_dor(:, Nt+1:end);                       % Nl by Np_tot*Nn
-
-% calculate channel coeffs
-ch_f_dor = y_dor_t./y_block_t;                      % Nt by Np_tot*Nn
-
-% calculate the average on the channel coeffs
-ch_f_dor_av = sum(ch_f_dor.').'/Nt;                 % 1 by Np_tot*Nn
-
-% compensate the channel
-y_dor_comp = y_dor_l./repmat(ch_f_dor_av, 1, Nl);   % Nl by Np_tot*Nn
-
-% reshape package
-y_comp = reshape(permute(...                        % Nn by Np_tot*Nl
-    reshape(y_dor_comp, Nn, [], Nl), [1 3 2]), Nn, []);
-
-% Equalizer End %
-%%%%%%%%%%%%%%%%%
+% equalize the received signal with the zero forced equalizer
+y_comp = eq_zf(y_demod_ofdm, y_block_t, Nn, Nt, Nl);
 
 % qam demodulation
 [y y_qam_r] = qam_demod(y_comp, N);
