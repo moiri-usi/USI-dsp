35e8d2f7a7b2e69cc60355626c2863b96ab565ea
Simon Maurer <moirelein@gmail.com>
2013-05-28 10:09:54 +0200

    structure of project

---
+++ project/ofdm_experiment_acoustic_channel.m
@@ -0,0 +1,31 @@
+% mesure noise at mic
+
+% mesure sampling ferquency missmatch
+
+% get bitstream from input file
+
+% normaloze bitstream (add zeroes)
+
+% generate training stream
+
+% qam modulation of data stream
+
+% qam modulation of training stream
+
+% generation of ofdm package
+
+% ofdm modulation
+
+% send through the channel
+
+% ofdm demodulation
+
+% equalize
+
+% qam demodulation
+
+% calculate BER
+
+% calculate SER
+
+% save bitstream to output file
